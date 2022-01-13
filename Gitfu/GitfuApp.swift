40a5c693df9ab18be8922f83b2e2b079bc839122
Pedro J. Fernandez <esquillidev@gmail.com>
2022-01-13 10:48:41 -0500

    Initial Commit

---
+++ Gitfu/GitfuApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitfuApp.swift
+//  Gitfu
+//
+//  Created by Pedro Fernandez on 1/13/22.
+//
+
+import SwiftUI
+
+@main
+struct GitfuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
